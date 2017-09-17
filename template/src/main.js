bfb0e5125c629c67e40a8c2fe5e3b8bb10dfa0ac
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2017-09-17 19:59:24 -0300

    Import and Install vue-head if user confirm

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import router from './router'
+{{#vueHead}}
+import VueHead from 'vue-head'
+{{/vueHead}}
 import { VueExtendLayout, layout } from 'vue-extend-layout'
 
 Vue.config.productionTip = false
+{{#vueHead}}
+Vue.use(VueHead)
+{{/vueHead}}
 Vue.use(VueExtendLayout)
 
 /* eslint-disable no-new */
